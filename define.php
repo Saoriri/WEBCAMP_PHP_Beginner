33edc19a798a141c00aa281e6493368d6dd3815c
Saoriri <Saoriri@example.com>
2023-03-19 06:49:28 +0000

    operator.phpを書いた

--- define.php
+++ define.php
@@ -1,10 +1,10 @@
 <?php
-/* 定数の定義 */
-define('DEF_INT', 123); // 整数
-define('DEF_FLOAT', 3.14); // 小数
-define('DEF_STRING', "string"); // 文字列
+$num_string = "999";
+$num_int = 999;
 
-// 定数の使用
-var_dump(DEF_INT);
-var_dump(DEF_FLOAT);
-var_dump(DEF_STRING);
+var_dump($num_string);
+var_dump($num_int);
+
+define("TEST_FLOAT" , 1.23);
+
+var_dump(TEST_FLOAT);
