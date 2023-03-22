8dfa6c6421209e7e5dbe07fc5178ec4a13b7776b
Saoriri <Saoriri@example.com>
2023-03-22 13:17:44 +0000

    math.phpを書いた

--- explode_implode.php
+++ explode_implode.php
@@ -1,12 +1,11 @@
 <?php
-$base_string = "Stollen;Panettone;Pandoro;Kouglof;Lussekatter";
-echo "元の文字列 \n";
-var_dump($base_string);
-
-$exploded_array = explode(";", $base_string);
-echo "\n explodeした配列　\n";
-var_dump($exploded_array);
-
-$imploded_string = implode(",",$exploded_array);
-echo "\n implodeした配列　\n";
-var_dump($imploded_string);
+$colors = [
+    "Red" => "#ff0000",
+    "Yellow" =>  "#ffff00",
+    "Green" =>  "#00ff00",
+    "Blue" =>  "#0000ff",
+    "Black" =>  "#000000",
+];
+foreach($colors as $k => $v) {
+    echo "{$k} -> {$v} \n";
+}
