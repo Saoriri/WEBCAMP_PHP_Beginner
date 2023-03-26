ba222592263644ba1c4523aec3c96927d59620a9
Saoriri <Saoriri@example.com>
2023-03-26 12:25:34 +0000

    cookie.phpを書いた

---
+++ cookie.php
@@ -0,0 +1,15 @@
+<?php
+function h(string $s) : string
+{
+    return htmlspecialchars($s,ENT_QUOTES);
+}
+
+$counter = intval($_COOKIE["counter"] ?? 1);
+
+echo "あなたがこのサイトを訪れたのは",h(strval ($counter)),"回目ですね。";
+
+$next_counter = strval($counter + 1);
+
+setcookie("counter" , $next_counter);
+
+
