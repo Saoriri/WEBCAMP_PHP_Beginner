674453e952f649d2ae7149a6cc3829f117c11728
Saoriri <Saoriri@example.com>
2023-03-28 13:52:49 +0000

    WEBCAMP_Log.php log_use.php log_use_2.phpを書いた

---
+++ WEBCAMP_Log.php
@@ -0,0 +1,10 @@
+<?php
+namespace WEBCAMP;
+
+class Log
+{
+    public function test()
+    {
+        echo "WEBCAMP内　Log内　test \n";
+    }
+}
---
+++ log_use.php
@@ -0,0 +1,5 @@
+<?php
+require_once( __DIR__ . "/WEBCAMP_Log.php" );
+
+$obj = new \WEBCAMP\Log();
+$obj->test();
