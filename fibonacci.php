1b0b608ff109f24cf37a6ffc8efda399e7216626
Saoriri <Saoriri@example.com>
2023-04-05 15:57:45 +0000

    課題提出

---
+++ fibonacci.php
@@ -0,0 +1,20 @@
+<?php
+
+$num1 = 0;
+$num2 = 1;
+
+echo "$num1 \n";
+echo "$num2 \n";
+
+while(($num3 = $num1 + $num2) < 10000) {
+
+   echo $num3 = $num1 + $num2;
+
+   $num1 = $num2;
+   $num2 = $num3;
+
+   echo "\n";
+}
+
+   echo "$num3 \n";
+?>
